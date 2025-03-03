8e24df22866fab06a8142c47ee32bc23c92bdf56
shaddydevops <122651230+shaddydevops@users.noreply.github.com>
2025-03-03 18:30:21 +0000

    adding output for public ip

--- infrastructure/modules/ec2/main.tf
+++ infrastructure/modules/ec2/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "web" {
   instance_type   = var.instance_type
   key_name        = var.key_name
   security_groups = [aws_security_group.ec2_sg.name]  # Attach security group
+  associate_public_ip_address = true
 
   tags = {
     Name = "clms-ec2"
--- infrastructure/modules/ec2/outputs.tf
+++ infrastructure/modules/ec2/outputs.tf
@@ -1,3 +1,8 @@
 output "ec2_instance_public_ip" {
   value = aws_instance.web.public_ip
 }
+
+# modules/ec2/outputs.tf
+output "public_ip" {
+  value = aws_instance.your_ec2_resource_name.public_ip
+}
