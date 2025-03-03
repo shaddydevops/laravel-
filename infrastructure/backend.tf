2fe54665a053306b1bd85ee6fb712389e0bf5af6
shaddydevops <122651230+shaddydevops@users.noreply.github.com>
2025-03-03 13:03:54 +0000

    creating a backend.tf for s3

---
+++ infrastructure/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "shadrack-terraform-state-bucket"  # Replace with your bucket name
+    key    = "terraform.tfstate"           # Path to the state file in the bucket
+    region = "us-east-1"                   # Region of the bucket
+  }
+}
